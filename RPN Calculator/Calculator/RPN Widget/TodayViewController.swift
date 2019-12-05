cf92f647b4b4eac72a916aeb806532a34c800df4
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-12-04 23:27:39 -0500

    Completed MVP

--- RPN Calculator/Calculator/RPN Widget/TodayViewController.swift
+++ RPN Calculator/Calculator/RPN Widget/TodayViewController.swift
@@ -8,12 +8,46 @@
 
 import UIKit
 import NotificationCenter
+import RPN
 
-class TodayViewController: UIViewController, NCWidgetProviding {
+class TodayViewController: UIViewController, NCWidgetProviding, UITextFieldDelegate {
+    
+    @IBOutlet var textField: UITextField!
+    
+    private let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.allowsFloats = true
+        formatter.maximumIntegerDigits = 20
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 20
         
+        return formatter
+    }()
+    
+    private var calculator = Calculator() {
+        didSet {
+            if let value = calculator.topValue {
+                textField.text = numberFormatter.string(from: value as NSNumber)
+            } else {
+                textField.text = ""
+            }
+        }
+    }
+    
+    private var digitAccumulator = DigitAccumulator() {
+        didSet {
+            if let value = digitAccumulator.value() {
+                textField.text = numberFormatter.string(from: value as NSNumber)
+            } else {
+                textField.text = ""
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+        self.textField.delegate = self
     }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
@@ -24,6 +58,57 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If there's an update, use NCUpdateResult.NewData
         
         completionHandler(NCUpdateResult.newData)
+    }
+    
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        switch activeDisplayMode {
+        case .compact:
+            preferredContentSize = maxSize
+            view.isHidden = true
+        case .expanded:
+            preferredContentSize = CGSize(width: maxSize.width, height: 370)
+            view.isHidden = false
+        @unknown default:
+            break
+        }
+    }
+    
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+        try? digitAccumulator.add(digit: .number(sender.tag))
+    }
+    
+    @IBAction func decimalButtonTapped(_ sender: UIButton) {
+        try? digitAccumulator.add(digit: .decimalPoint)
+    }
+    
+    @IBAction func returnButtonTapped(_ sender: UIButton) {
+        // Take the value from the accumualtor and push it to the stack
+        if let value = digitAccumulator.value() {
+            calculator.push(number: value)
+        }
+        digitAccumulator.clear()
+    }
+    
+    @IBAction func divideButtonTapped(_ sender: UIButton) {
+        calculator.push(operator: .divide)
+    }
+    
+    @IBAction func multiplyButtonTapped(_ sender: UIButton) {
+        calculator.push(operator: .multiply)
+    }
+    
+    @IBAction func subtractButtonTapped(_ sender: UIButton) {
+        calculator.push(operator: .subtract)
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        calculator.push(operator: .add)
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        calculator.clear()
+        digitAccumulator.clear()
+        return true
     }
     
 }
